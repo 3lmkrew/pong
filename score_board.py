780e475f4a025074075bbcb4d92e35c0d004b52a
Mason <94991037+3lmkrew@users.noreply.github.com>
2021-11-24 15:59:20 -0800

    Update score_board.py

--- score_board.py
+++ score_board.py
@@ -17,11 +17,11 @@ class ScoreBoard(turtle.Turtle):
         self.goto(-100, 230)
         self.write(self.l_score, align="center", font=("Courier", 50, "normal"))
         self.goto(-100, 225)
-        self.write("|__|", align="center", font=("Courier", 25, "normal"))
+        self.write("____", align="center", font=("Courier", 25, "normal"))
         self.goto(100, 230)
         self.write(self.r_score, align="center", font=("Courier", 50, "normal"))
         self.goto(100, 225)
-        self.write("|__|", align="center", font=("Courier", 25, "normal"))
+        self.write("____", align="center", font=("Courier", 25, "normal"))
         self.goto(0, 0)
         for y_axis in range(-500, 500, 40): # create a dashed line in the center of game
             self.goto(0, y_axis)
